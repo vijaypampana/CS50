b2a540b2be4d26d105518de10d830c3cd919b26a
vpampana <vpampana@payveris.com>
2020-06-12 11:48:08 -0400

    Adding bootstrap utilities

--- src/main/python/app.py
+++ src/main/python/app.py
@@ -5,16 +5,19 @@ app = Flask(__name__)   #A new web application is a flask application, app is a 
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route("/jsonBeautifier")
 def jsonBeautifier():
     return render_template("jsonBeautifier.html")
 
-@app.route("/xmlBeautifier")
+@app.route('/xmlBeautifier')
 def xmlBeautifier():
-    return "Welcome to XML Beautifier Page"
+    return render_template("xmlBeautifier.html")
 
 @app.route("/createUser")
 def createUser():
     return "Welcome to Create Automated User Page"
+
+if __name__ == "__main__":
+    app.run()
